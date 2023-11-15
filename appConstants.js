f9e7f9d633ded7557b142812f1d4b2954c6a3cb9
Mahesh Hiremath <mhiremat@stevens.edu>
2023-11-15 17:22:17 -0500

    validationUtils, repairs.js, constants class
    changes related to new util class for validations
    data/repairs.js separated and refactored
    new appConstants class

--- appConstants.js
+++ appConstants.js
@@ -8,7 +8,18 @@ const constants = {
     tag: 20,
     password: 8,
     first_last_names: 20,
+    address: 100,
+    deviceType: 20,
+    manufacturer: 20,
+    modelName: 20,
+    modelNumber: 20,
+    serialNumber: 20,
+    issue: 200,
+    stepsTaken: 200,
+    workToBeDone: 200,
+    conditionOfDevice: 200,
   },
+  repairStatus: ["Not Started", "In Progress", "Completed"],
 };
 
 export default constants;
