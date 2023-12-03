9e6d2f0070ce6d814a7ef694939b87f99a3cdcf3
Mahesh Hiremath <mhiremat@stevens.edu>
2023-12-03 03:52:09 -0500

    nodemailer feature
    configured oAuth 2.0 in google console

--- back-end/data/repairs.js
+++ back-end/data/repairs.js
@@ -1,6 +1,9 @@
 import { ObjectId } from "mongodb";
 import { clients } from "../config/mongoCollections.js";
 import emailValidator from "email-validator";
+import { getClientById } from "./clients.js";
+import axios from "axios";
+import fs from "fs";
 import {
   validateString,
   validatePhoneNumber,
@@ -8,6 +11,24 @@ import {
   validateEmail,
 } from "../util/validationUtil.js";
 import constants from "../../appConstants.js";
+import { sendEmail } from "../nodemailer/sendMailService.js";
+
+export const getDeviceById = async (clientId, deviceId) => {
+  validateString(deviceId, "Device ID");
+  validateString(clientId, "Client ID");
+
+  if (!ObjectId.isValid(deviceId)) throw "Invalid Device ID format";
+  if (!ObjectId.isValid(clientId)) throw "Invalid Client ID format";
+
+  let client = await getClientById(clientId);
+
+  if (!client) throw "No client with that ID";
+
+  let device = client.Devices.find(
+    (device) => device._id.toString() === deviceId
+  );
+  return device;
+};
 
 export const createRepair = async (clientId, deviceID, workOrder) => {
   clientId = validateObjectId(clientId, "Client ID");
@@ -83,7 +104,45 @@ export const createRepair = async (clientId, deviceID, workOrder) => {
   if (!updatedInfo.acknowledged || updatedInfo.modifiedCount === 0) {
     throw "Could not update client successfully";
   }
-  return newRepair;
+  if (newRepair) {
+    let client = await getClientById(clientId);
+    let device = client.Devices.find(
+      (device) => device._id.toString() === deviceID
+    );
+
+    let reportData = newRepair;
+    reportData.clientName = client.name;
+    reportData.clientEmail = client.email;
+    reportData.clientPhone = client.phoneNumber;
+    reportData.clientAddress = client.address;
+
+    reportData.deviceType = device.deviceType;
+    reportData.manufacturer = device.manufacturer;
+    reportData.modelName = device.modelName;
+    reportData.modelNumber = device.modelNumber;
+    reportData.serialNumber = device.serialNumber;
+
+    try {
+      const response = await axios.post(
+        "http://localhost:5488/api/report",
+        {
+          template: { name: "check-in" },
+          data: reportData,
+        },
+        {
+          responseType: "blob",
+        }
+      );
+
+      const pdfFilename = `repair-report-${newRepair._id}.pdf`;
+      fs.writeFileSync(pdfFilename, response.data);
+
+      return pdfFilename;
+    } catch (error) {
+      console.error("Error generating report:", error);
+      throw error;
+    }
+  }
 };
 
 export const getWorkorderById = async (repairId) => {
@@ -119,6 +178,7 @@ export const updateWorkorderAfterRepair = async (
 
   let repair = await getWorkorderById(repairID);
   if (repair === null) throw "No repair with that id";
+  const repairStatus = wasTheRepairSuccessful ? "Completed" : "In Progress";
 
   const updateFilter = {
     _id: new ObjectId(repair.clientID),
@@ -129,7 +189,7 @@ export const updateWorkorderAfterRepair = async (
     $set: {
       "Repairs.$.repairTechnicianNotes": repairNotes,
       "Repairs.$.wasTheRepairSuccessful": wasTheRepairSuccessful,
-      "Repairs.$.repairStatus": "Ready to be Picked Up",
+      "Repairs.$.repairStatus": repairStatus,
       "Repairs.$.isDevicePickedUpAlready": false,
       "Repairs.$.repairCompletionDate": new Date(),
     },
@@ -144,6 +204,24 @@ export const updateWorkorderAfterRepair = async (
   if (!updatedInfo.acknowledged || updatedInfo.modifiedCount === 0) {
     throw "Could not update the repair successfully";
   }
+  const updatedRepair = await getWorkorderById(repairID);
+
+  if (wasTheRepairSuccessful) {
+    try {
+      const client = await getClientById(updatedRepair.clientID);
+      if (!client) throw "No client with that ID";
+
+      await sendEmail(
+        client.email,
+        "Repair Order Completed",
+        `Your repair with ID: ${repairID} is completed.`,
+        `<p>Your repair with ID: ${repairID} is completed.</p>`
+      );
+    } catch (error) {
+      console.error("Failed to send email:", error);
+    }
+  }
+
   return await getWorkorderById(repairID);
 };
 
