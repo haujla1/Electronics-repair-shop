84db4d3c6bded73d9f9081160a387cc9192c4b41
cassiama <cassiama@stevens.edu>
2023-12-12 17:11:22 -0500

    removed images for devices

--- front-end/src/components/clientDetailsPage/clientDevices.jsx
+++ front-end/src/components/clientDetailsPage/clientDevices.jsx
@@ -1,16 +1,6 @@
 import React, {useContext, useEffect, useState} from "react";
 import {Link} from 'react-router-dom';
 import axios from "axios";
-import stockPhoneWhiteIcon from '../../assets/android/android-phone-white.svg';
-import stockPhoneBlackIcon from '../../assets/android/android-phone-black.svg';
-import stockTabletWhiteIcon from '../../assets/android/android-tablet-white.svg';
-import stockTabletBlackIcon from '../../assets/android/android-tablet-black.svg';
-import iphoneWhiteIcon from '../../assets/apple/iphone-white.svg';
-import iphoneBlackIcon from '../../assets/apple/iphone-black.svg';
-import ipadWhiteIcon from '../../assets/apple/ipad-white.svg';
-import ipadBlackIcon from '../../assets/apple/ipad-black.svg';
-import macbookWhiteIcon from '../../assets/apple/macbook-white.svg';
-import macbookBlackIcon from '../../assets/apple/macbook-black.svg';
 import AddDevice from './addDevice.jsx'
 
 
@@ -51,9 +41,6 @@ function Devices({clientId}){
     return (
         <>
             <h3>Devices</h3>
-            {/* <p>Devices for {clientId} here</p>
-            <p>Cards for each device, with image or stock image of device type (i.e. if iphone 8 get a pic of iphone 8 somehow). Then when clicked on opens the modal with repair history (and links to full repair info) and button to create a new repair.</p> */}
-
             <div>
                 {
                     loading
@@ -69,24 +56,6 @@ function Devices({clientId}){
                                         return (
                                             <div className="card" key={device._id}>
                                                 <h5>{device.manufacturer} {device.modelName}</h5>
-                                                {
-                                                    device.manufacturer.toLowerCase() === 'apple'
-                                                        ? 
-                                                            (
-                                                                device.deviceType === 'mobile'
-                                                                    ?
-                                                                        <img src={iphoneWhiteIcon} alt="iPhone" width="50%"/>
-                                                                    :
-                                                                        <img src={ipadWhiteIcon} alt="iPad" width="50%" />
-                                                            )
-                                                        : (
-                                                            device.deviceType === 'mobile'
-                                                                ?
-                                                                    <img src={stockPhoneWhiteIcon} alt={`${device.manufacturer} Phone`} width="50%"/>
-                                                                :
-                                                                    <img src={stockTabletWhiteIcon} alt={`${device.manufacturer} Tablet`} width="50%"/>
-                                                        )
-                                                }
                                                 <dl>
                                                     <dt>Model Number:</dt>
                                                     <dd>{device.modelNumber}</dd>
