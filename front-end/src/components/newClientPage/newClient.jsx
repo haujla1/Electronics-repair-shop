e486fa7c8cbe5d3aa2f2db06c5c4a265df9ccfca
cassiama <cassiama@stevens.edu>
2023-12-15 16:19:11 -0500

    fixed error handling and added type checking for newClient

--- front-end/src/components/newClientPage/newClient.jsx
+++ front-end/src/components/newClientPage/newClient.jsx
@@ -9,17 +9,10 @@ import constants from "../../../../back-end/appConstants.js";
 //note from kyle -- the import was wrong: forgot to go into back-end
 
 function NewClient(){
-    const [formData, setFormData] = useState({
-        firstName: '',
-        lastName: '',
-        phoneNumber: '',
-        email: '',
-        address: '',
-        age: null
-    });
+    const [error, setError] = useState('');
 
     const addClient = async (form) => {
-        console.log(form);
+        // console.log(form);
         try {
             let { data } = await axios.post('http://localhost:3000/clients', form);
             return data;
@@ -32,88 +25,103 @@ function NewClient(){
 
     const handleSubmit = async (e) => {
         e.preventDefault();
-        let firstName = document.getElementById('firstName').value;
-        let lastName = document.getElementById('lastName').value;
-        let phoneNumber = document.getElementById('phoneNumber').value;
-        let email = document.getElementById('email').value;
-        let address = document.getElementById('address').value;
-        let age = document.getElementById('age').value;
+        const {
+            firstName: firstNameElem,
+            lastName: lastNameElem,
+            phoneNumber: phoneNumberElem,
+            email: emailElem,
+            address: addressElem,
+            age: ageElem
+        } = e.target.elements;
+
+        // Protect against changes through inspect element.
+        if (
+            firstNameElem.getAttribute('type') !== 'text' ||
+            lastNameElem.getAttribute('type') !== 'text' ||
+            phoneNumberElem.getAttribute('type') !== 'text' ||
+            emailElem.getAttribute('type') !== 'email' ||
+            addressElem.getAttribute('type') !== 'text' ||
+            ageElem.getAttribute('type') !== 'number'
+        ) {
+            setError('Invalid Input Type');
+            let errorSpans = document.getElementsByClassName('error');
+            errorSpans.forEach(errorSpan => {
+                console.log(errorSpan);
+                errorSpan.textContent = '';
+            });
+            return;
+        }
+
+        let firstName = firstNameElem.value;
+        let lastName = lastNameElem.value;
+        let phoneNumber = phoneNumberElem.value;
+        let email = emailElem.value;
+        let address = addressElem.value;
+        let age = ageElem.value;
 
         // Do the form validation.
         let validForm = false;
         let errors = [];
         try {
+            if (firstName.length === 0) throw 'First Name must be provided';
             if (firstName.length > constants.stringLimits.first_last_names)
                 throw `First Name cannot be longer than ${constants.stringLimits.first_last_names} characters`;
             firstName = firstName.trim();
             if (firstName.length === 0) throw 'First Name cannot be empty';
-            document.getElementById('firstName').nextElementSibling.textContent = '';
         } catch (e) {
             errors.push(e);
-            let errorSpan = document.getElementById('firstName').nextElementSibling;
-            errorSpan.textContent = e;
         }
 
         try {
+            if (lastName.length === 0) throw 'Last Name must be provided';
             if (lastName.length > constants.stringLimits.first_last_names)
                 throw `Last Name cannot be longer than ${constants.stringLimits.first_last_names} characters`;
             lastName = lastName.trim();
             if (lastName.length === 0) throw 'Last Name cannot be empty';
-            document.getElementById('lastName').nextElementSibling.textContent = '';
         } catch (e) {
             errors.push(e);
-            let errorSpan = document.getElementById('lastName').nextElementSibling;
-            errorSpan.textContent = e;
         }
 
         try {
-            if (phoneNumber.includes('-')) phoneNumber = phoneNumber.split('-').join('');
+            if (phoneNumber.length === 0) throw 'Phone Number must be provided';
             phoneNumber = phoneNumber.trim();
             if (phoneNumber.length !== 10)
               throw "Phone number must be 10 digits long";
             if (!/^\d+$/.test(phoneNumber))
               throw "Phone number must contain only digits";
-            document.getElementById('phoneNumber').nextElementSibling.textContent = '';
         } catch (e) {
             errors.push(e);
-            let errorSpan = document.getElementById('phoneNumber').nextElementSibling;
-            errorSpan.textContent = e;
         }
 
         try {
+            if (email.length === 0) throw 'Email must be provided';
             email = email.trim();
             if (email.length === 0) throw 'Email cannot be empty';
-            document.getElementById('email').nextElementSibling.textContent = '';
         } catch (e) {
             errors.push(e);
-            let errorSpan = document.getElementById('email').nextElementSibling;
-            errorSpan.textContent = e;
         }
 
         try {
+            if (address.length === 0) throw 'Address must be provided';
             if (address.length > constants.stringLimits.address)
                 throw `Address cannot be longer than ${constants.stringLimits.address} characters`;
             address = address.trim();
             if (address.length === 0) throw 'Address cannot be empty';
-            document.getElementById('address').nextElementSibling.textContent = '';
         } catch (e) {
             errors.push(e);
-            let errorSpan = document.getElementById('address').nextElementSibling;
-            errorSpan.textContent = e;
         }
 
         try {
+            if (age.length === 0) throw 'Age must be provided';
             if (isNaN(age)) throw 'Age is not a number';
             age = Number.parseInt(age, 10);
             if (age < constants.min_age || age > constants.max_age)
                 throw `Age must be between ${constants.min_age} and ${constants.max_age} years old`;
-            document.getElementById('age').nextElementSibling.textContent = '';
         } catch (e) {
             errors.push(e);
-            let errorSpan = document.getElementById('age').nextElementSibling;
-            errorSpan.textContent = e;
         }
 
+        // console.log(errors);
         validForm = errors.length === 0;
 
         if (validForm) {
@@ -125,52 +133,22 @@ function NewClient(){
                 address: address,
                 age: age
             };
-            console.log(form);
-            setFormData(form);
+            // console.log(form);
 
             let data = await addClient(form);
             console.log(data);
             if (data.error) {
-                if (data.error.includes('First Name')) {
-                    let errorSpan = document.getElementById('firstName').nextElementSibling;
-                    errorSpan.textContent = e;
-                }
-                else if (data.error.includes('Last Name')) {
-                    let errorSpan = document.getElementById('lastName').nextElementSibling;
-                    errorSpan.textContent = e;
-                }
-                else if (data.error.includes('Phone number')) {
-                    let errorSpan = document.getElementById('phoneNumber').nextElementSibling;
-                    errorSpan.textContent = e;
-                }
-                else if (data.error.includes('Email')) {
-                    let errorSpan = document.getElementById('email').nextElementSibling;
-                    errorSpan.textContent = data.error;
-                }
-                else if (data.error.includes('Address')) {
-                    let errorSpan = document.getElementById('address').nextElementSibling;
-                    errorSpan.textContent = e;
-                }
-                else {
-                    let errorSpan = document.getElementById('age').nextElementSibling;
-                    errorSpan.textContent = e;
-                }
+                setError(data.error);
                 return;
             }
 
             document.getElementById('add-client').reset();
-            for (let id of Object.keys(formData)) document.getElementById(id).nextElementSibling.textContent = '';
+            setError('');
             alert('Client Added');
-            setFormData({
-                firstName: '',
-                lastName: '',
-                phoneNumber: '',
-                email: '',
-                address: '',
-                age: null
-            });
             return;
         } else {
+            if (typeof errors[0] === 'undefined') setError('Unknown Error')
+            else setError(errors[0]);
             return;
         }
     }
@@ -185,7 +163,6 @@ function NewClient(){
                             First Name:
                             <br />
                             <input type="text" id="firstName" required autoFocus={true} />
-                            <span className="error"></span>
                         </label>
                     </div>
 
@@ -194,7 +171,6 @@ function NewClient(){
                             Last Name:
                             <br />
                             <input type="text" id="lastName" required />
-                            <span className="error"></span>
                         </label>
                     </div>
 
@@ -203,7 +179,6 @@ function NewClient(){
                             Phone Number:
                             <br />
                             <input type="text" id="phoneNumber" required />
-                            <span className="error"></span>
                         </label>
                     </div>
 
@@ -211,8 +186,7 @@ function NewClient(){
                         <label>
                             Email:
                             <br />
-                            <input type="text" id="email" required />
-                            <span className="error"></span>
+                            <input type="email" id="email" required />
                         </label>
                     </div>
 
@@ -221,7 +195,6 @@ function NewClient(){
                             Address:
                             <br />
                             <input type="text" id="address" required />
-                            <span className="error"></span>
                         </label>
                     </div>
 
@@ -230,13 +203,14 @@ function NewClient(){
                             Age:
                             <br />
                             <input type="number" id="age" required min={constants.min_age} max={constants.max_age} />
-                            <span className="error"></span>
                         </label>
                     </div>
                     
                     <button type="submit">
                         Add Client
                     </button>
+                    <br />
+                    <p>{error}</p>
                 </form>
             </div>
         </>
